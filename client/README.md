0dff2c6ba67a1fd468a0657f6e364c99b5d197eb
Github-Archives <drewreynerwilliams@gmail.com>
2022-05-07 15:25:43 -0400

    FE BE talking

--- client/README.md
+++ client/README.md
@@ -1,3 +1,7 @@
+# Reminder
+
+Be sure to start BE then FE for app to work
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+
+const [backendData, setBackendData] = useState([{}])
+
+useEffect(() => {
+  fetch("/api").then(
+    response => response.json()
+  ).then(
+    data => {
+      setBackendData(data)
+    }
+  )
+}, [])
+
   return (
     <div>App</div>
   )
